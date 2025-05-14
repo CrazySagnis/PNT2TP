be6a8d6411cd6a04e17c753cf4e801f19a338e16
CrazySagnis <CrazySagnis@gmail.com>
2025-05-13 21:17:20 -0300

    VUE 3 COMPOSITION API
    Se cambia la sintaxis por la pedida por el profesor.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
     <h2>Hola, soy el home</h2>
+    <button @click="irAlLogin">Ir al Login</button>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'Home',
+<script setup>
+import { useRouter } from 'vue-router'
+const router = useRouter()
+function irAlLogin() {
+  router.push('/login')
 }
 </script>
