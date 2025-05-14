7f5baab5e7742e705780a8eef1b26a30a87faacf
CrazySagnis <CrazySagnis@gmail.com>
2025-05-13 22:56:16 -0300

    Base API y Productos
    Se hace una base de API y demostracion de display de productos como prueba.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,33 @@
 <template>
-  <div>
-    <h2>Hola, soy el home</h2>
-    <button @click="irAlLogin">Ir al Login</button>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-6 mb-4" v-for="item in items" :key="item.id">
+        <div class="card h-100">
+          <img :src="item.imagen" class="card-img-top" alt="imagen" />
+          <div class="card-body">
+            <h5 class="card-title">{{ item.modelo }}</h5>
+            <h5 class="card-title">{{ `$ ${item.precio_ars}` }}</h5>
+            <p class="card-text">{{ item.descripcion }}</p>
+            <a href="#" class="btn btn-primary">Ver más</a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router'
-const router = useRouter()
-function irAlLogin() {
-  router.push('/login')
-}
+import { ref, onMounted } from 'vue'
+
+const items = ref([])
+
+onMounted(async () => {
+  try {
+    const res = await fetch('https://www.mockachino.com/69b724dc-dd10-4e/lugar2')
+    const data = await res.json()
+    items.value = data.PlacaVideo9070Xt
+  } catch (error) {
+    console.error('Error al traer los datos:', error)
+  }
+})
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -8,9 +8,7 @@ const routes = [
   { path: '/home', component: Home },
 ]
 
-const router = createRouter({
+export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
-
-export default router
