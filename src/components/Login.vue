ff426ef514348fb82190857f2d63b3327e6540b4
CrazySagnis <CrazySagnis@gmail.com>
2025-05-19 22:35:33 -0300

    usuario activo y login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,33 +4,45 @@
     <input v-model="username" placeholder="Usuario" />
     <input v-model="password" type="password" placeholder="Contraseña" />
     <button @click="goHome">Ir a Home</button>
+    <button @click="logIn">Iniciar Sesión</button>
+    <p v-if="errorMessage" style="color: red">{{ errorMessage }}</p>
   </div>
 </template>
 
 <script setup>
 import { useRouter } from 'vue-router'
 import { ref } from 'vue'
+
 const router = useRouter()
 const username = ref('')
 const password = ref('')
+const errorMessage = ref('')
+const users = ref([])
+
 function goHome() {
   router.push('/home')
 }
-</script>
 
-<!-- <script>
-export default {
-  name: 'Login',
-  data() {
-    return {
-      username: '',
-      password: '',
+async function logIn() {
+  errorMessage.value = ''
+  try {
+    const response = await fetch('https://www.mockachino.com/69b724dc-dd10-4e/users')
+    const data = await response.json()
+    users.value = data.users
+
+    const user = users.value.find(
+      (u) => u.username === username.value && u.password === password.value,
+    )
+
+    if (user) {
+      router.push('/home')
+      localStorage.setItem('usuarioActivo', JSON.stringify(user))
+    } else {
+      errorMessage.value = 'Usuario o contraseña incorrectos'
     }
-  },
-  methods: {
-    goHome() {
-      this.$router.push('/home')
-    },
-  },
+  } catch (error) {
+    errorMessage.value = 'Error al conectar con el servidor'
+    console.error(error)
+  }
 }
-</script> -->
+</script>
